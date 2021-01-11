f0ce9225fcade6931b41d3c76d26042b8207648f
Giovanni Bagolin <giovannibagolin@outlook.it>
2021-01-11 17:48:18 +0100

    Modified to try deploing.
    Signed-off-by: Giovanni Bagolin <giovannibagolin@outlook.it>

--- src/app.py
+++ src/app.py
@@ -6,13 +6,14 @@ from sanic_cors import CORS, cross_origin
 app = Sanic("server")
 CORS(app)
 
-@app.route("/api", methods=["POST"])
-def post_json(request):
-    return json({ "received": "true", "message": request.json})
+# @app.route("/api", methods=["POST"])
+# def post_json(request):
+#     rule = request.json 
+#     return json(rule)
 
-# @app.route('/api')
-# def handle_request(request):
-#     return response.text('Hello world!')
+@app.route('/api')
+def handle_request(request):
+    return response.text('Hello world!')
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8000)
+    app.run(debug=True)

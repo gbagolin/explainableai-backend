dd46309f66cb9999175f884fd9c24737dc963567
Giovanni Bagolin <giovannibagolin@outlook.it>
2021-01-11 19:06:01 +0100

    Deployed, per davvero.

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
-from sanic import Sanic
-from sanic.response import json
-from sanic import response
-from sanic_cors import CORS, cross_origin
-
-app = Sanic(__name__)
-CORS(app)
-
-# @app.route("/api", methods=["POST"])
-# def post_json(request):
-#     rule = request.json 
-#     return json(rule)
-
-@app.route('/api')
-def handle_request(request):
-    return response.text('Hello world!')
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
-def handle_request(request):
-    return response.text('Hello world!')
+def hello():
+    return "Hello World!"
 
-app.run(host='127.0.0.1', port=1337, workers=1)
+if __name__ == '__main__':
+    app.run()

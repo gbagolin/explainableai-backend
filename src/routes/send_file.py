61bd2ee345eeb8f383cbe58bf501a9da05ffa958
gbagolin <giovannibagolin@outlook.it>
2021-06-29 22:04:20 +0200

    Anomalies distribution plot now gets created and added to the report.

--- src/routes/send_file.py
+++ src/routes/send_file.py
@@ -1,10 +1,18 @@
+import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 from fastapi import APIRouter, Request, Response
 from fastapi.responses import FileResponse
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from os import path
+from PIL import Image
+import base64
+import io
 import weasyprint
 from time import gmtime, strftime
+from typing import List
+from . import var
 
 path_str = path.dirname(path.realpath(__file__))
 print(path_str)
@@ -12,97 +20,66 @@ print(path_str)
 router = APIRouter()
 templates = Jinja2Templates(directory="src/template")
 
-# list of anomalies same action
-anomalies_sa = [
-    {
-        "action": "listen",
-        "beliefs": [
-            {
-                "belief": 0.9694691535150646,
-                "state": "tiger left"
-            },
-            {
-                "belief": 0.030530846484935436,
-                "state": "tiger right"
-            }
-        ],
-        "hellinger_distance": 0.15927953822858362,
-        "is_anomaly": "true",
-        "run": "run 48",
-        "step": 4
-    },
-    {
-        "action": "listen",
-        "beliefs": [
-            {
-                "belief": 0.9694691535150646,
-                "state": "tiger left"
-            },
-            {
-                "belief": 0.030530846484935436,
-                "state": "tiger right"
-            }
-        ],
-        "hellinger_distance": 0.15927953822858362,
-        "is_anomaly": "true",
-        "run": "run 48",
-        "step": 4
-    },
 
-]
+def extract_statistics(anomalies_same_action: List) -> List:
+    '''
 
-anomalies_da = [
-    {
-        "action": "listen",
-        "beliefs": [
-            {
-                "belief": 0.9694691535150646,
-                "state": "tiger left"
-            },
-            {
-                "belief": 0.030530846484935436,
-                "state": "tiger right"
-            }
-        ],
-        "hellinger_distance": 0.15927953822858362,
-        "is_anomaly": "true",
-        "run": "run 48",
-        "step": 4
-    },
-    {
-        "action": "listen",
-        "beliefs": [
-            {
-                "belief": 0.9694691535150646,
-                "state": "tiger left"
-            },
-            {
-                "belief": 0.030530846484935436,
-                "state": "tiger right"
-            }
-        ],
-        "hellinger_distance": 0.15927953822858362,
-        "is_anomaly": "true",
-        "run": "run 48",
-        "step": 4
-    },
+    :param anomalies_same_action: List, of all anomalies whose action is equal to the action analyzed.
+    :return: numpy.array[10]
 
-]
+    arr[i] contains the number of anomalies whose (i / 10) <= severity (i / 10) + 0.1
+    This function is used as support function to plot the graph.
+    '''
+    # get the list of severities for each severity in the list of anomalies same action.
+    severities = [anom['hellinger_distance'] for anom in anomalies_same_action]
+    arr = numpy.asarray(severities)
+    # num_seve_per_dec[i] contains the number of anomalies whose i <= severity < i + 0.1
+    num_seve_per_dec = np.zeros(10)
+    left = 0
+    pos = 0
+    space = np.arange(0.0, 1.0, 0.1)
+    for dec in space:
+        right = dec + 0.1
+        cond = numpy.logical_and(arr >= left, arr < right)
+        num_seve_per_dec[pos] = len(arr[cond])
+        left = right
+        pos += 1
 
+    return num_seve_per_dec
+
+def plot_distribution(distr: List) -> None:
+    for pos, elem in enumerate(distr):
+        plt.bar(pos,height=elem, width=1, align='edge')
+
+    plt.xticks(np.arange(10),labels=np.arange(0,1,0.10).round(1))
+    plt.xlabel("severity")
+    plt.ylabel("#anomalies")
+    plt.title("Anomalies distribution for severity")
+    plt.savefig('distribution.jpg')
+    plt.close()
 
 @router.get('/api/send_file')
 async def main():
-    headers = {"Content-Disposition": "attachment;",
-               "filename": "google.pdf;"}
+    # headers = {"Content-Disposition": "attachment;",
+    #            "filename": "google.pdf;"}
+    headers = {}
     pdf = weasyprint.HTML('http://localhost:8001/create_report').write_pdf()
-    open('google.pdf', 'wb').write(pdf)
-    return FileResponse("google.pdf", headers=headers)
+    open('report.pdf', 'wb').write(pdf)
+    distr = extract_statistics(var.anomalies_sa)
+    plot_distribution(distr)
+    return FileResponse("report.pdf", headers=headers)
 
 
 @router.get("/create_report", response_class=HTMLResponse)
 async def read_item(request: Request):
     now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    google_logo = Image.open("distribution.jpg")
+    # google_logo.show()
+    data = io.BytesIO()
+    google_logo.save(data, "JPEG")
+    encoded_img_data = base64.b64encode(data.getvalue())
     return templates.TemplateResponse("report.html", {"request": request,
                                                       "now": now,
-                                                      "anomalies_sa": anomalies_sa,
-                                                      "anomalies_da": anomalies_da})
+                                                      "anomalies_sa": var.anomalies_sa,
+                                                      "anomalies_da": var.anomalies_sa,
+                                                      "img_data": encoded_img_data.decode('utf-8')})
---
+++ src/routes/var.py
@@ -0,0 +1,291 @@
+anomalies_sa = [
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.9694691535150646,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.030530846484935436,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.16217479553800762,
+        "is_anomaly": "true",
+        "run": "run 48",
+        "step": 4
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8550522224921059,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.1449477775078941,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.010544920537430412,
+        "is_anomaly": "false",
+        "run": "run 29",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8543273350471294,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.1456726649528706,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.009817706427590566,
+        "is_anomaly": "false",
+        "run": "run 4",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.85366934717913,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.14633065282087002,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.009158902837056967,
+        "is_anomaly": "false",
+        "run": "run 34",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8535635077623929,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.14643649223760716,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.009053046735801878,
+        "is_anomaly": "false",
+        "run": "run 77",
+        "step": 3
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8535243622294765,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.14647563777052353,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.009013903048268825,
+        "is_anomaly": "false",
+        "run": "run 63",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8521820448877806,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.14781795511221946,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.007674253763240209,
+        "is_anomaly": "false",
+        "run": "run 50",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8515227648752988,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.14847723512470126,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.00701812387942586,
+        "is_anomaly": "false",
+        "run": "run 22",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8501310895677093,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.14986891043229073,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.0056370280548280515,
+        "is_anomaly": "false",
+        "run": "run 48",
+        "step": 3
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8499517723655655,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.15004822763443454,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.005459458729300575,
+        "is_anomaly": "false",
+        "run": "run 7",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8498590513543326,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.15014094864566735,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.005367675744988865,
+        "is_anomaly": "false",
+        "run": "run 25",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8495178019045309,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.15048219809546914,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.005030079169336085,
+        "is_anomaly": "false",
+        "run": "run 40",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.849260003654303,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.15073999634569707,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.004775249130569066,
+        "is_anomaly": "false",
+        "run": "run 52",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8489875579409613,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.1510124420590388,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.004506134822382812,
+        "is_anomaly": "false",
+        "run": "run 71",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8487469505433577,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.15125304945664228,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.004268635296689885,
+        "is_anomaly": "false",
+        "run": "run 9",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.8484755911284902,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.15152440887150975,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.004000967013065159,
+        "is_anomaly": "false",
+        "run": "run 84",
+        "step": 1
+    },
+    {
+        "action": "listen",
+        "beliefs": [
+            {
+                "belief": 0.848032315319175,
+                "state": "tiger left"
+            },
+            {
+                "belief": 0.15196768468082503,
+                "state": "tiger right"
+            }
+        ],
+        "hellinger_distance": 0.003564143558245107,
+        "is_anomaly": "false",
+        "run": "run 53",
+        "step": 1
+    }
+]
